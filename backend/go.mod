d9228ed9c03a6edc36d9b0f8907898c074f1fef1
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-29 00:05:19 +0545

    go backend completed for now

--- backend/go.mod
+++ backend/go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -19,6 +20,7 @@ require (
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/gorilla/handlers v1.5.2
 	github.com/gorilla/mux v1.8.1
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/sajin-shrestha/form_go/database"
-	"github.com/sajin-shrestha/form_go/handlers"
+
+	customHandlers "github.com/sajin-shrestha/form_go/handlers"
 	"github.com/sajin-shrestha/form_go/middleware"
 )
 
@@ -20,12 +22,15 @@ func main() {
 
 	database.ConnectDB()
 
+	corsOptions := handlers.AllowedOrigins([]string{"http://localhost:5173"})
+	corsHandler := handlers.CORS(corsOptions)
+
 	router := mux.NewRouter()
-	router.HandleFunc("/register", handlers.Register).Methods("POST")
-	router.HandleFunc("/login", handlers.Login).Methods("POST")
-	router.HandleFunc("/delete/{id}", middleware.AuthMiddleware(handlers.DeleteUser)).Methods("DELETE")
-	router.HandleFunc("/home", middleware.AuthMiddleware(handlers.Home)).Methods("GET")
+	router.HandleFunc("/register", customHandlers.Register).Methods("POST")
+	router.HandleFunc("/login", customHandlers.Login).Methods("POST")
+	router.HandleFunc("/delete/{id}", middleware.AuthMiddleware(customHandlers.DeleteUser)).Methods("DELETE")
+	router.HandleFunc("/home", middleware.AuthMiddleware(customHandlers.Home)).Methods("GET")
 
 	log.Println("server running on port: " + os.Getenv("PORT"))
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), corsHandler(router)))
 }
