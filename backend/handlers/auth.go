2123a798d812a7913686f152c5af117608703505
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-28 22:27:22 +0545

    auth handler completed

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/sajin-shrestha/form_go/database"
 	"github.com/sajin-shrestha/form_go/models"
+	"golang.org/x/crypto/bcrypt"
 )
-
 
 var jwtKey = []byte("my_secret_jwt_key")
 
@@ -20,5 +22,53 @@ func Register(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	json.NewDecoder(r.Body).Decode(&user)
 
-	hashedPassword, err := 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	user.Password = string(hashedPassword)
+
+	if err := database.DB.Create(&user).Error; err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	var dbUser models.User
+	if err := database.DB.Where("username = ?", user.Username).First(&dbUser).Error; err != nil {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password)); err != nil {
+		http.Error(w, "invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(5 * time.Hour)
+	claims := &Claims{
+		Username: user.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
 }
